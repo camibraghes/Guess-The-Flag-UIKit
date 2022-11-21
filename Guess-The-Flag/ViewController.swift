55eec389a262ee5401511b1a1138eb61202fd1b9
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-21 11:25:48 +0200

    Add UIButton and CALayer

--- Guess-The-Flag/ViewController.swift
+++ Guess-The-Flag/ViewController.swift
@@ -13,11 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet var secondButton: UIButton!
     @IBOutlet var thirdButton: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "Uk", "Us"]
+        askQuestion()
+        
+        firstButton.layer.borderWidth = 1
+        firstButton.layer.borderColor = UIColor.gray.cgColor
+        
+        secondButton.layer.borderWidth = 1
+        secondButton.layer.borderColor = UIColor.gray.cgColor
+        
+        thirdButton.layer.borderWidth = 1
+        thirdButton.layer.borderColor = UIColor.gray.cgColor
     }
-
+    
+    func askQuestion() {
+        firstButton.setImage(UIImage(named: countries[0]), for: .normal)
+        secondButton.setImage(UIImage(named: countries[1]), for: .normal)
+        thirdButton.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
